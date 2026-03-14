0df186a42e6caed0c044b5445072314796ebc35a
_111y <huy264128@gmail.com>
2026-03-14 16:22:44 +0800

    setup shizuku libs and AndroidMenifest

--- platform/shizuku_service/build.gradle.kts
+++ platform/shizuku_service/build.gradle.kts
@@ -1,13 +1,19 @@
 plugins {
-    alias(libs.plugins.android.library)
+    alias(libs.plugins.android.application)
 }
 
 android {
     namespace = "com.projectmaidgroup.platform.shizuku_service"
-    compileSdk = 36
+    compileSdk {
+        version = release(36)
+    }
 
     defaultConfig {
+        applicationId = "com.projectmaidgroup.platform.shizuku_service"
         minSdk = 31
+        targetSdk = 36
+        versionCode = 1
+        versionName = "1.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
